a3ee1702ccb7b3adcaa14fcf23984ddc750bcc56
GONG-YI LIAO <gong-yi@GongTop.(none)>
2010-11-10 20:31:24 -0500

    1. add plot to Q1 2. updates on Q2    a. add bike.csv data file    b. the setting of y's and z's.

--- assign3.R
+++ assign3.R
@@ -1,4 +1,4 @@
-## Last update Tue Nov  9 22:50:03 2010 GONG-YI LIAO
+## Last update Wed Nov 10 20:05:05 2010 GONG-YI LIAO
 
 require(MCMCpack)
 require(arm)
@@ -19,8 +19,49 @@ hist(α.2 - α.1, prob=TRUE, main=expression("Histogram of " * alpha[2] - alpha[
 lines(density(α.2 - α.1), type="l", col="red")
 dev.off()
 
-## Q4
+rm(ξ, α.1, α.2, π.1, π.2, y1, y2)
 
+## Q2, q8 on page 98, BDA textbook
+
+## original data set
+## bicycles <- c(16, 9, 10, 13, 19, 20, 18, 17, 35, 55,
+##               12, 1, 2, 4, 9, 7, 9, 8, NA, NA,
+##               8, 35, 31, 19, 38, 47, 44, 44, 29, 18,
+##               10, 43, 5, 14, 58, 15, 0, 47, 51, 32,
+##               60, 51, 58, 59, 53, 68, 68, 60, 71, 63,
+##               8, 9, 6, 9, 19, 61, 31, 75, 14, 25)
+##
+## vehicles <- c(58, 90, 48, 57, 103, 57, 86, 112, 273, 64,
+##               113, 18, 14, 44, 208, 67, 29, 154, NA, NA,
+##               29, 415, 425, 42, 180, 675, 620, 437, 47, 462,
+##               557, 1258, 499, 601, 1163, 700, 90, 1093, 1459, 1086,
+##               1545, 1499, 1598, 503, 407, 1494, 1558, 1706, 476, 752,
+##               1248, 1246, 1596, 1765, 1290, 2498, 2346, 3101, 1918, 2318)
+##
+## traffic.counting <- data.frame(type=c(rep("Residential", 20), rep("Fairly busy", 20), rep("Busy", 20)),
+##                                bike.route=rep(c(rep(TRUE, 10), rep(FALSE, 10)), 3),
+##                                bicycles=bicycles, vehicles=vehicles)
+##
+## write.csv(traffic.counting, 'bike.csv', row.names=FALSE)
+## rm(bicycles, vehicles, traffic.counting)
+
+bike.data <- read.csv('bike.csv', header=TRUE)
+
+y1 <- bike.data[1:10,3]/rowSums(bike.data[1:10, 3:4])
+z1 <- bike.data[11:18,3]/rowSums(bike.data[11:18, 3:4])
+
+
+
+## Q3, q12 on
+
+dose.fatal <- data.frame(dose=c(-0.86, -0.30, -0.05, 0.73),
+                         anomials=rep(5,4),
+                         death=c(0, 1, 3, 5))
+
+
+## Q4, designed by Prof. Chen
+
+## General functions
 post.inte.two.sample <- function(theta, obs) {
   def1 <- 1
   ## the reason that we need to use the inefficient loop
@@ -107,6 +148,7 @@ plot(seq(-9, 13, length=20000),
 lines(density(post.sample.c.ii), col="blue", lty=2)
 dev.off()
 
+## Q5, q11 on page 155, BDA textbook.
 
 
 
